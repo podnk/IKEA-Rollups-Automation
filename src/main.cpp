8378746fff94bd88b06932c50b9f42a44cefc07c
Denis Lada <podnklada@gmail.com>
2020-05-06 22:34:36 +0300

    v1
    Just four functions:
    1. Stop
    2. Close
    3. Open
    4. Return to prev pos

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,29 @@
+/*
+    step.stop();                          // Остановка
+    step.moveTo(-step.currentPosition()); // Переместиться в изначальную позицию отосительно текущей
+    step.moveTo(closedBlinds);            // закрыть роллапы
+    step.moveTo(0);                       // Переместиться в начальную позицию
+    step.setCurrentPosition(0);           // Установить текущую позицию в качестве начальной
+    step.run();                           // Запуск действий
+*/
+
 #include <Arduino.h>
 #include <AccelStepper.h>
-#include <GyverButton.h>
 
 #define IN1 8
 #define IN2 9
 #define IN3 10
 #define IN4 11
+#define A 2
+#define B 3
+#define C 4
+#define D 5
 #define closedBlinds 48000
-
-GButton btn(2);
 
 AccelStepper step(8, IN1, IN3, IN2, IN4);
 
-void isr()
-{
-    btn.tick();
-}
-
 void setup()
 {
-    attachInterrupt(1, isr, CHANGE);
     step.setMaxSpeed(1000.0);
     step.setAcceleration(500.0);
     step.setSpeed(1000);
@@ -27,27 +31,21 @@ void setup()
 
 void loop()
 {
-    btn.tick();
-
-    if (btn.isSingle())
-    {
-        step.stop();
-    }
-    else if (btn.isDouble())
-    {
-        step.moveTo(-step.currentPosition());
-    }
-    else if (btn.isTriple())
+    if (digitalRead(A) == 1)
     {
         step.moveTo(closedBlinds);
     }
-    else if (btn.isHolded())
+    else if (digitalRead(B) == 1)
     {
         step.moveTo(0);
     }
-    else if (btn.getClicks() == 4)
+    else if (digitalRead(C) == 1)
     {
-        step.setCurrentPosition(0);
+        step.stop();
+    }
+    else if (digitalRead(D) == 1)
+    {
+        step.moveTo(-step.currentPosition());
     }
 
     step.run();
